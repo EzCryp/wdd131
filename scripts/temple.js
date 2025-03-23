94c94fa7ccb4f193c7ac94f16354573e29c02fa8
Ezcryp <u637424283@byui.edu>
2025-03-23 19:38:02 +0800

    css and js update

---
+++ scripts/temple.js
@@ -0,0 +1,13 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Header 1 - Hamburguer Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
