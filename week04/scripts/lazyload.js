81765954174a5022b40bb7c7f3962ad859435a2b
Ezcryp <u637424283@byui.edu>
2025-04-17 06:57:12 +0800

    W04 Lazy Load

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,9 @@
+// Footer
+document.addEventListener('DOMContentLoaded', () => {
+    const yearElement = document.querySelector("#currentyear");
+    const lastModifiedElement = document.querySelector("#lastModified");
+
+    yearElement.textContent = `${new Date().getFullYear()}`;
+    lastModifiedElement.textContent = ` ${document.lastModified}`;
+    lastModifiedElement.classList.add('last-modified');
+});
